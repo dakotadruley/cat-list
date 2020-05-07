0023927969cc26d0a7597a7879b85e12743688b3
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-05-06 18:06:42 -0700

    commented out the styles for now

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Header from '../Header/Header.js';
 import CatCalling from '../../hooks/CatCalling.js';
-import AppDark from './AppDark.css';
-import AppLight from './AppLight.css';
+// import AppDark from './AppDark.css';
+// import AppLight from './AppLight.css';
 
 export default function App() {
-  const theme = useTheme();
-  
+  // const theme = useTheme();
+
   return (
     <>
       <Header />
